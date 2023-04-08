ebdd5a919464f51e7d102e049ae4c359dd422471
lordtgm <lordtgm@gmail.com>
2023-04-08 20:06:17 +0330

    fixed problems with darwin os

--- src/main/java/com/github/lordtgm/minestom/TpsFixExtension.java
+++ src/main/java/com/github/lordtgm/minestom/TpsFixExtension.java
@@ -50,7 +50,7 @@ public class TpsFixExtension extends Extension {
     }
 
     public static boolean isWindows() {
-        return System.getProperty("os.name").toLowerCase().contains("win");
+        return System.getProperty("os.name").toLowerCase().startsWith("win");
     }
 
 }
